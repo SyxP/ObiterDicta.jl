60fd2963817ee7e2a2a64156ee953d22f2930843
Syx Pek <20557013+SyxP@users.noreply.github.com>
2023-11-30 09:14:19 +0800

    Speedup downloads

--- src/DownloadBundles.jl
+++ src/DownloadBundles.jl
@@ -70,6 +70,8 @@ function DownloadAllBundles(bundleLocation = "$git_download_cache/Bundles/")
     
     URLs = parseCatalog()
     for url in URLs
+        filePath, _ = getFilePathFromBundleURL(url, bundleLocation)
+        isfile(filePath) && continue
         DownloadBundle(url, bundleLocation)
         sleep(0.2) # To not overwhelm the server
     end
